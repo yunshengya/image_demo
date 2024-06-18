16b652e011d10a41c0f2fd2e1bc7b36e9d2a85fd
云升 <1617428415@qq.com>
2024-06-18 16:11:46 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,21 @@
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
 </script>
 <style  lang="scss">
-@import '/src/resource/css/global.scss'
+// @import '/src/resource/css/global.scss'
+$scrollbar-width: 6px; // 设置滚动条的宽度
+$scrollbar-thumb-color: #f0f0f0; // 设置滚动条滑块的颜色
+$scrollbar-track-color: #ffffff; // 设置滚动条轨道的颜色
+
+// 全局滚动条样式
+* {
+	&::-webkit-scrollbar {
+		width: $scrollbar-width;
+		background-color: $scrollbar-track-color;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background-color: $scrollbar-thumb-color;
+		border-radius: 12px;
+	}
+}
 </style>
